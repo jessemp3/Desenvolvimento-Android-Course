d1af9360e1f3876ed47f329e8e8952ef145d5cba
jesse <nitchsx@gmail.com>
2025-10-02 03:52:25 -0300

    feat: add functionality to select 'pedra' and verify winner

--- PedraPapelTesoura/app/src/main/java/com/jesse/pedrapapeltesoura/MainActivity.java
+++ PedraPapelTesoura/app/src/main/java/com/jesse/pedrapapeltesoura/MainActivity.java
@@ -1,6 +1,7 @@
 package com.jesse.pedrapapeltesoura;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void selecionarPedra(View view ){
+        verificarGanhador("pedra");
+    }
+
+    private void verificarGanhador(String escolhaUsuario){
+
     }
 }
