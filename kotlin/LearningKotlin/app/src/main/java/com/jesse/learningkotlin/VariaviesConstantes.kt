574eca015046deb614152ff5752217900c3936d7
jesse <nitchsx@gmail.com>
2025-10-21 01:21:12 -0300

    feat: initialize LearningKotlin project with basic structure and resources

---
+++ kotlin/LearningKotlin/app/src/main/java/com/jesse/learningkotlin/VariaviesConstantes.kt
@@ -0,0 +1,10 @@
+package com.jesse.learningkotlin
+
+const val MAX_AGE: Int = 77 // const assim fora , server pra definir constantes globais
+
+fun main() {
+    val name: String = "Jesse"
+    var name2 = "kaique"
+
+    println("Hello, $name and $name2!")
+}
