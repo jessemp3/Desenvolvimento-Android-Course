f6217734123ceb4cd0b3505c064eca9d1be27e5e
jesse <nitchsx@gmail.com>
2025-10-21 00:35:22 -0300

    feat: implement password input and login button in EditTextActivity2

--- ComponentesDeInterface/app/src/main/java/com/jesse/componentesdeinterface/EditTextActivity2.java
+++ ComponentesDeInterface/app/src/main/java/com/jesse/componentesdeinterface/EditTextActivity2.java
@@ -1,6 +1,10 @@
 package com.jesse.componentesdeinterface;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.jesse.componentesdeinterface.databinding.ActivityEditText2Binding;
+import com.jesse.componentesdeinterface.databinding.ActivityMainBinding;
+
 public class EditTextActivity2 extends AppCompatActivity {
+        private ActivityEditText2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityEditText2Binding.inflate(getLayoutInflater());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_edit_text2);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+
+        Button button = binding.buttonLogin;
+        EditText text = binding.editTextPassword;
+
+        button.setOnClickListener(v -> {
+            Toast.makeText(this, "Senha - " + text.getText(), Toast.LENGTH_SHORT).show();
+        });
+    }}
