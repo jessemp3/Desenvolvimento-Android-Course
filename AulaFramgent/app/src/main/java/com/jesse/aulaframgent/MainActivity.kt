7b8186793a8b5d8c57ddb812e1bca1c49449f203
jesse <nitchsx@gmail.com>
2025-11-03 03:36:57 -0300

    feat: add ChamadasFragment and integrate with MainActivity for fragment navigation

--- AulaFramgent/app/src/main/java/com/jesse/aulaframgent/MainActivity.kt
+++ AulaFramgent/app/src/main/java/com/jesse/aulaframgent/MainActivity.kt
@@ -5,16 +5,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jesse.aulaframgent.databinding.ActivityMainBinding
+import com.jesse.aulaframgent.fragments.ChamadasFragment
+import com.jesse.aulaframgent.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        // começo da transação de fragment
+//        val fragmentManager = supportFragmentManager.beginTransaction()
+//
+//        //alteraões no fragment
+//        fragmentManager.add(
+//            R.id.fragmentContainerView, // binding não funciona aqui
+//            ConversasFragment()
+//        )
+//
+//        fragmentManager.commit()
+
+
+        binding.button.setOnClickListener {
+            //forma simplificada
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragmentContainerView,
+                    ConversasFragment()
+                ) // o metodo add simplismente colocar na tela , se já tiver aberto um fragment ele sobrepõe, o melhor é usar o replace que substitui
+                .commit()
+        }
+
+        binding.button2.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragmentContainerView,
+                    ChamadasFragment()
+                )
+                .commit()
+        }
+
+
+
+
+
     }
 }
+
---
+++ AulaFramgent/app/src/main/java/com/jesse/aulaframgent/fragments/ChamadasFragment.kt
@@ -0,0 +1,23 @@
+package com.jesse.aulaframgent.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ChamadasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate( // inflando o layout do fragment
+            com.jesse.aulaframgent.R.layout.fragment_chamadas,
+            container,
+            false
+        )
+    }
+
+}
