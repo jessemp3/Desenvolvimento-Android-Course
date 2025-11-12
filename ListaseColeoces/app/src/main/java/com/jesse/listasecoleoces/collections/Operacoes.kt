7a127861c13a4160e45bfed28e3481faffa62063
jesse <nitchsx@gmail.com>
2025-11-12 13:59:06 -0300

    feat: add map, union, and ordenacao functions in Operacoes.kt

--- ListaseColeoces/app/src/main/java/com/jesse/listasecoleoces/collections/Operacoes.kt
+++ ListaseColeoces/app/src/main/java/com/jesse/listasecoleoces/collections/Operacoes.kt
@@ -10,3 +10,30 @@ fun filter(){
         fruta == "laranja"
     }
 }
+
+/*
+cria uma lista com o mesmo número de itens ou os itens transformados
+ */
+fun map(){
+    val lista = listOf("jesse", "kaique" , "cice")
+
+    val novalista = lista.map { nome -> // it.uppercase()
+        nome.uppercase()
+    }
+}
+
+
+// faz a junção de itens de duas listas
+fun union(){
+    val listaLanches = listOf("mclancheFeliz" , "refri")
+    val filme = listOf("pipoca" , "refri")
+
+    val novalista = listaLanches.union(filme)
+}
+
+
+// asc -> ascedente
+// desc -> descendente
+fun ordenacao(){
+
+}
