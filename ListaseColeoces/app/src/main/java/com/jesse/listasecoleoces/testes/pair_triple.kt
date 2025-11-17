da9f9a9d52d68bbbad2564cd23fc0cdeec94a58b
jesse <nitchsx@gmail.com>
2025-11-17 14:18:30 -0300

    feat: add example usage of Pair and Triple in pair_triple.kt

---
+++ ListaseColeoces/app/src/main/java/com/jesse/listasecoleoces/testes/pair_triple.kt
@@ -0,0 +1,12 @@
+package com.jesse.listasecoleoces.testes
+
+fun main() {
+
+    val localization = Pair(10,20) // pair pode ser escrito assim -> 10 to 20
+    val timer = Triple(14,15, 12)
+
+    println(localization.first)
+    println(localization.second)
+
+    println(timer.third)
+}
