d5e7a4a8cb7937f1ecce0ce7d2b3d158d3edc6e4
jesse <nitchsx@gmail.com>
2025-11-20 14:21:24 -0300

    feat: add LinearLayoutuActivity and layout for displaying a linear layout with buttons

---
+++ ListaseColeoces/app/src/main/java/com/jesse/listasecoleoces/linearLayout/LinearLayoutuActivity.kt
@@ -0,0 +1,21 @@
+package com.jesse.listasecoleoces.linearLayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jesse.listasecoleoces.R
+
+class LinearLayoutuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_linear_layoutu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
