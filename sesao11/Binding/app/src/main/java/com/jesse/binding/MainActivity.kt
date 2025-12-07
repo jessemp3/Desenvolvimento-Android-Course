4072dff8a852fba98dc33d3d07f983d93346d212
jesse <nitchsx@gmail.com>
2025-12-07 14:43:15 -0300

    feat: implement view binding and update landscape layout
    - Enable view binding in `MainActivity` using `ActivityMainBinding`.
    - Use lazy initialization for the binding object.
    - Implement click listeners for buttons using the binding object and a `with` scope function.
    - Add a null-safe click listener for `button2`, which only exists in the landscape layout.
    - Update `layout-land/activity_main.xml` to include a second button (`button2`) and adjust constraints for both buttons.

--- sesao11/Binding/app/src/main/java/com/jesse/binding/MainActivity.kt
+++ sesao11/Binding/app/src/main/java/com/jesse/binding/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.jesse.binding
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jesse.binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //forma 1 de chamar
+//    lateinit var binding: ActivityMainBinding
+
+    //forma 2
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // forma 1
+//        binding.button.setOnClickListener {
+//            Toast.makeText(this , "Clicou no botão" , Toast.LENGTH_SHORT).show()
+//        }
+
+        //forma 2
+        with(binding){ // funcaõ de escopo
+            button.setOnClickListener {
+                Toast.makeText(this@MainActivity , "Clicou no botão do with" , Toast.LENGTH_SHORT).show()
+            }
+            button2?.setOnClickListener { // button2 existe em um layout e em outro não , por isso ele pede a chamada segura
+                Toast.makeText(this@MainActivity , "Clicou no botão 2 do with" , Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
