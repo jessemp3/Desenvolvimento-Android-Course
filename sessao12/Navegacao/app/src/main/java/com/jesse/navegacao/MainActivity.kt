deb520911e6c43a7c33480941b10eeee455a9886
jesse <nitchsx@gmail.com>
2025-12-15 23:39:16 -0300

    feat: implement Options Menu in MainActivity
    - Create a new Android project named "Navegacao".
    - Define a menu resource `menu_princiapl.xml` with items for "Adicionar", "Pesquisar", "Configurações", and "Sair".
    - Add custom vector drawables `ic_add.xml` and `ic_search.xml` for menu icons.
    - Inflate the menu in `MainActivity` by overriding `onCreateOptionsMenu`.
    - Implement `onOptionsItemSelected` to handle clicks on menu items, displaying a `Toast` for each action and finishing the activity for the "Sair" item.

---
+++ sessao12/Navegacao/app/src/main/java/com/jesse/navegacao/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.jesse.navegacao
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+    // forma de esconder a actionbar
+    //        supportActionBar?.hide()
+    }
+
+
+    // quando a tela é carregada , esse metodo é chamado e ele infla um layout pra contruir o menu (se ouver)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        /*
+       aqui , o menu foi inflado , porem o itens ainda não são cliveis(só os 3 pontinhos né , obvio)
+         */
+        menuInflater.inflate(R.menu.menu_princiapl , menu)
+
+        return true
+    }
+
+    /*
+    metodo pra quando eu clicar nos itesn
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.itemPesquisar -> Toast.makeText(this , "Pesquisar" , Toast.LENGTH_SHORT).show()
+            R.id.itemConfiguracoes -> Toast.makeText(this , "Configurações" , Toast.LENGTH_SHORT).show()
+            R.id.itemAdicionar -> Toast.makeText(this , "Adicionar" , Toast.LENGTH_SHORT).show()
+            R.id.itemSair ->  finish()
+        }
+        return true
+    }
+
+
+}
