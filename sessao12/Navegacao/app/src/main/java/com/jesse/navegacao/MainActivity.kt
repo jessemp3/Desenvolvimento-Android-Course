316b852bfd39a9b28958c8c93550fa641a68c186
jesse <nitchsx@gmail.com>
2025-12-15 23:52:44 -0300

    refactor: use MenuProvider to create and handle menu items
    - Refactor `MainActivity` to use `MenuProvider` for creating the options menu, replacing the deprecated `onCreateOptionsMenu` and `onOptionsItemSelected` methods.
    - Create a new function `inicializarActionBar` that calls `addMenuProvider`.
    - Implement `onCreateMenu` to inflate the `R.menu.menu_princiapl` layout.
    - Implement `onMenuItemSelected` to handle click events for "Pesquisar", "Configurações", "Adicionar", and "Sair", displaying a `Toast` for each or finishing the activity.
    - Comment out the previous `onCreateOptionsMenu` and `onOptionsItemSelected` implementations.

--- sessao12/Navegacao/app/src/main/java/com/jesse/navegacao/MainActivity.kt
+++ sessao12/Navegacao/app/src/main/java/com/jesse/navegacao/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jesse.navegacao
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -22,31 +24,76 @@ class MainActivity : AppCompatActivity() {
         }
     // forma de esconder a actionbar
     //        supportActionBar?.hide()
+
+        inicializarActionBar()
+    }
+
+    /*
+    usando menu provider, outra forma de criar os menus
+     */
+    private fun inicializarActionBar() {
+        addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(
+                    menu: Menu,
+                    menuInflater: MenuInflater
+                ) {
+                    menuInflater.inflate(R.menu.menu_princiapl, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    when (menuItem.itemId) {
+                        R.id.itemPesquisar -> Toast.makeText(
+                            this@MainActivity,
+                            "Pesquisar",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        R.id.itemConfiguracoes -> Toast.makeText(
+                            this@MainActivity,
+                            "Configurações",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        R.id.itemAdicionar -> Toast.makeText(
+                            this@MainActivity,
+                            "Adicionar",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        R.id.itemSair -> finish()
+                    }
+                    return true
+                }
+
+            }
+
+        )
     }
 
 
     // quando a tela é carregada , esse metodo é chamado e ele infla um layout pra contruir o menu (se ouver)
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        /*
-       aqui , o menu foi inflado , porem o itens ainda não são cliveis(só os 3 pontinhos né , obvio)
-         */
-        menuInflater.inflate(R.menu.menu_princiapl , menu)
-
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        /*
+//       aqui , o menu foi inflado , porem o itens ainda não são cliveis(só os 3 pontinhos né , obvio)
+//         */
+//        menuInflater.inflate(R.menu.menu_princiapl , menu)
+//
+//        return true
+//    }
 
     /*
     metodo pra quando eu clicar nos itesn
      */
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.itemPesquisar -> Toast.makeText(this , "Pesquisar" , Toast.LENGTH_SHORT).show()
-            R.id.itemConfiguracoes -> Toast.makeText(this , "Configurações" , Toast.LENGTH_SHORT).show()
-            R.id.itemAdicionar -> Toast.makeText(this , "Adicionar" , Toast.LENGTH_SHORT).show()
-            R.id.itemSair ->  finish()
-        }
-        return true
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//            R.id.itemPesquisar -> Toast.makeText(this , "Pesquisar" , Toast.LENGTH_SHORT).show()
+//            R.id.itemConfiguracoes -> Toast.makeText(this , "Configurações" , Toast.LENGTH_SHORT).show()
+//            R.id.itemAdicionar -> Toast.makeText(this , "Adicionar" , Toast.LENGTH_SHORT).show()
+//            R.id.itemSair ->  finish()
+//        }
+//        return true
+//    }
 
 
 }
