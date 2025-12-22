02fdce7663a71d4ac45fae9c523a63aeb56c55fc
jesse <nitchsx@gmail.com>
2025-12-22 02:57:10 -0300

    feat: add "add task" screen and update main layout
    - Create `AdicionarTarefaActivity` to handle the addition of new tasks.
    - Add `activity_adicionar_tarefa.xml` layout, which includes an `EditText` for the task description and a "Salvar" button.
    - Register `AdicionarTarefaActivity` in the `AndroidManifest.xml`.
    - Update `activity_main.xml` to use a `RecyclerView` for displaying tasks and add a `FloatingActionButton` to navigate to the new "add task" screen.
    - Add `ic_add`, `ic_edit`, and `ic_delete` vector drawables for UI actions.
    - Include a new layout file `item_tarefa.xml` to define the visual representation of a single task item in the list.
    - Add the `recyclerview` dependency to `build.gradle.kts` and `libs.versions.toml`.

---
+++ taskList/app/src/main/java/com/jesse/tasklist/AdicionarTarefaActivity.kt
@@ -0,0 +1,20 @@
+package com.jesse.tasklist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdicionarTarefaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_adicionar_tarefa)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
