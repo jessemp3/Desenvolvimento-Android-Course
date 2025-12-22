175c55fd1119b9656fb9b5f0524844ca23b26feb
jesse <nitchsx@gmail.com>
2025-12-22 03:12:42 -0300

    feat: implement navigation and update UI components
    - Implement View Binding in `MainActivity` and `AdicionarTarefaActivity` to replace `findViewById`.
    - Add an `onClickListener` to the Floating Action Button (`fabAdicionar`) in `MainActivity` to navigate to `AdicionarTarefaActivity` using an `Intent`.
    - Update the base application theme in `themes.xml` to inherit from `Theme.MaterialComponents.DayNight` and `Theme.Material3.DayNight`, respectively.
    - Refine the `FloatingActionButton` in `activity_main.xml` by setting its ID to `fabAdicionar` and customizing its background tint and ripple color.
    - Adjust the "Salvar" button in `activity_adicionar_tarefa.xml` by setting its text size to `15sp`.

--- taskList/app/src/main/java/com/jesse/tasklist/AdicionarTarefaActivity.kt
+++ taskList/app/src/main/java/com/jesse/tasklist/AdicionarTarefaActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jesse.tasklist.databinding.ActivityAdicionarTarefaBinding
 
 class AdicionarTarefaActivity : AppCompatActivity() {
+    private val binding by lazy{
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_adicionar_tarefa)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        with(binding){
+
         }
     }
 }
