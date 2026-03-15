1f6ee23a952bcd5d8a1b9268c2ad2257cf85c76f
jesse <nitchsx@gmail.com>
2026-03-15 02:35:42 -0300

    feat: update hardware requirements and improve permission handling
    - Update `AndroidManifest.xml` to make `android.hardware.camera` a required feature.
    - Replace `READ_MEDIA_IMAGES` with `READ_EXTERNAL_STORAGE` (limited to API 32) and update camera permission declarations in the manifest.
    - Refactor `requisitarPermisoes` in `Permissao.kt` to check for granted permissions before requesting.
    - Implement logic in `Permissao.kt` to only request permissions that have not already been granted by the user.

--- ClassFirebase/app/src/main/java/com/jesse/classfirebase/helper/Permissao.kt
+++ ClassFirebase/app/src/main/java/com/jesse/classfirebase/helper/Permissao.kt
@@ -1,16 +1,41 @@
 package com.jesse.classfirebase.helper
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class Permissao {
-    companion object{
-        fun requisitarPermisoes(activity: Activity , permisoes: List<String>){
-            ActivityCompat.requestPermissions(
-                 activity,
-                permisoes.toTypedArray(),
-                0
+    companion object {
+        fun requisitarPermisoes(activity: Activity, permisoes: List<String>) {
+
+
+            //checar se tem permissao
+            ContextCompat.checkSelfPermission(
+                activity,
+                permisoes.first()
             )
+
+            val permisoesNegadas = mutableListOf<String>()
+            permisoes.forEach { permisao ->
+                val temPermissao = ContextCompat.checkSelfPermission(
+                    activity, permisao
+                ) == PackageManager.PERMISSION_GRANTED
+
+
+                if (!temPermissao) {
+                    permisoesNegadas.add(permisao)
+                }
+            }
+
+            if (permisoesNegadas.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    permisoesNegadas.toTypedArray(),
+                    0
+                )
+            }
+
         }
     }
 }
