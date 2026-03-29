e6f36a7f909cda87b2cf26288e2ce7db31a639f0
jesse <nitchsx@gmail.com>
2026-03-29 03:33:23 -0300

    feat: implement runtime permission handling for camera and gallery access
    - Update `AndroidManifest.xml` to require `android.hardware.camera` and declare permissions for `CAMERA`, `INTERNET`, and `READ_MEDIA_IMAGES`.
    - Add `solicitarPermisoes` method to `PerfilActivity` to check and request missing permissions at runtime.
    - Use `ActivityResultContracts.RequestMultiplePermissions` to handle permission requests for camera and media access.
    - Update `PerfilActivity` state with boolean flags `temPermisaoGaleria` and `temPermisaoCamera` based on user authorization.
    - Annotate `onCreate` and `solicitarPermisoes` with `@RequiresApi(Build.VERSION_CODES.TIRAMISU)` to support `READ_MEDIA_IMAGES`.

--- Whatsapp/app/src/main/java/com/jesse/whatsapp/PerfilActivity.kt
+++ Whatsapp/app/src/main/java/com/jesse/whatsapp/PerfilActivity.kt
@@ -1,8 +1,13 @@
 package com.jesse.whatsapp
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.jesse.whatsapp.databinding.ActivityPerfilBinding
@@ -13,6 +18,10 @@ class PerfilActivity : AppCompatActivity() {
         ActivityPerfilBinding.inflate(layoutInflater)
     }
 
+    private var temPermisaoGaleria = false
+    private var temPermisaoCamera = false
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,11 +32,52 @@ class PerfilActivity : AppCompatActivity() {
             insets
         }
 
+        solicitarPermisoes()
+
         binding.includePerfilToolbar.setup(
             title = "Perfil",
             onBack = {
                 finish()
             }
         )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun solicitarPermisoes() {
+        //verificar se já tem a permisao
+        temPermisaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermisaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+
+        // lista de negadas
+        val listaPermisoesNegadas = mutableListOf<String>()
+        if(!temPermisaoCamera ){
+            listaPermisoesNegadas.add(android.Manifest.permission.CAMERA)
+        }
+        if(!temPermisaoGaleria){
+            listaPermisoesNegadas.add(android.Manifest.permission.READ_MEDIA_IMAGES)
+        }
+
+        if(listaPermisoesNegadas.isNotEmpty()){
+            //solicitar multiplicar permisoes
+            val gerenciadorPermisoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ){permisoes ->
+                temPermisaoCamera = permisoes[android.Manifest.permission.CAMERA] ?: temPermisaoCamera
+                temPermisaoGaleria = permisoes[android.Manifest.permission.READ_MEDIA_IMAGES] ?: temPermisaoGaleria
+            }
+
+            gerenciadorPermisoes.launch(listaPermisoesNegadas.toTypedArray())
+        }
+
+
+
     }
 }
